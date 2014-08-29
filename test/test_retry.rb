92450541f31bf95cb3e34caf1492ed746a7c3b6e
Abdelkader Boudih <terminale@gmail.com>
2014-08-29 13:00:39 +0000

    Fix tests

--- test/test_retry.rb
+++ test/test_retry.rb
@@ -51,7 +51,7 @@ class TestRetry < Sidekiq::Test
     end
 
     it 'handles zany characters in error message, #1705' do
-      skip if RUBY_VERSION >= '2.1.0'
+      skip 'skipped! test requires ruby 2.1+' if RUBY_VERSION <= '2.1.0'
       @redis.expect :zadd, 1, ['retry', String, String]
       msg = { 'class' => 'Bob', 'args' => [1,2,'foo'], 'retry' => 2 }
       msg2 = msg.dup
